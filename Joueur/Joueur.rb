3f9d8d2d2fd706d701243827c6ad6fd469baaa27
Bastien3 <bastien.simon.etu@univ-lemans.fr>
2020-03-09 11:02:36 +0100

    commit de test

---
+++ Joueur/Joueur.rb
@@ -0,0 +1,18 @@
+
+class Joueur
+
+	#@argent : l'argent dont dispose le joueur
+	#@etoiles : le nombre d'étoiles dont dispose le joueur
+	#@parametres : table de hachage qui contient des paires cle => valeur correspondant à "nom paramètre" => ID
+	#@listeGrillesDebloquees : la liste des ID des grilles débloquées
+	#@listeObjetsDebloques : la liste des ID des objets débloqués par le joueur
+
+	def Joueur.creer()
+		new()
+	end
+	
+	def initialize()
+		
+	end
+
+end

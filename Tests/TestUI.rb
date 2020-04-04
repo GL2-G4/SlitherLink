0fcda59a74931e3e5bffc2f05eff00a89676d060
CPietJa <tibane.galbrun@gmail.com>
2020-04-04 02:03:45 +0200

    Update Couleur Ligne + Intersection  PartieUI

--- Tests/TestUI.rb
+++ Tests/TestUI.rb
@@ -4,15 +4,14 @@
 # File Created: Thursday, 2nd April 2020 3:29:53 pm
 # Author: <CPietJa>Galbrun T.
 # -----
-# Last Modified: Thursday, 2nd April 2020 4:25:17 pm
+# Last Modified: Friday, 3rd April 2020 10:53:12 pm
 # Modified By: <CPietJa>Galbrun T.
 #
 require "gtk3"
+Gtk.init # Ne pas le changer de place
 require_relative "../Noyau/Jeu.rb"
 require_relative "../UI/PartieUI.rb"
 require_relative "../Noyau/LoadSaveGrilles/ChargeurGrille.rb"
-
-Gtk.init
 
 chargeurGrille = ChargeurGrille.charger(File.dirname(__FILE__) + "/../Grilles/grille")
 grille = chargeurGrille.getGrilleIndex(0)

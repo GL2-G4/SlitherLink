1a73ade455389d7c5c8e5abc3119a708d895488d
SithMann <38142935+SithMann@users.noreply.github.com>
2020-04-10 14:44:14 +0200

    Liaison UI

--- Parametres/Theme.rb
+++ Parametres/Theme.rb
@@ -10,19 +10,19 @@ class Theme
 	#@debloque : booléen qui vaut vrai si le thème est débloqué, faux sinon
 	#@prix : le prix à payer pour débloquer le thème
 	
-	module Couleur
+	# module Couleur
 	
-		BLANC = 0
-		NOIR = 1
-		BLEU = 2
-		VERT = 3
-		JAUNE = 4
-		ROUGE = 5
+	# 	BLANC = 0
+	# 	NOIR = 1
+	# 	BLEU = 2
+	# 	VERT = 3
+	# 	JAUNE = 4
+	# 	ROUGE = 5
 		
-		extend OpConstante
-	end
+	# 	extend OpConstante
+	# end
 	
-	attr_reader :debloque, :prix
+	attr_reader :debloque, :prix, :nom
 	
 	# Création du thème
 	def Theme.charger(contenuFichier)
@@ -42,11 +42,12 @@ class Theme
 		contenu = contenuFichier.split(";")
 		
 		# Chargement des 3 couleurs, du booléen debloque et du prix
-		@couleur1 = self.toCouleur(contenu[0])
-		@couleur2 = self.toCouleur(contenu[1])
-		@couleur3 = self.toCouleur(contenu[2])
-		@debloque = self.toBooleen(contenu[3].chomp)
-		@prix = contenu[4].to_i
+		# @couleur1 = self.toCouleur(contenu[0])
+		# @couleur2 = self.toCouleur(contenu[1])
+		# @couleur3 = self.toCouleur(contenu[2])
+		@nom = contenu[0]
+		@debloque = self.toBooleen(contenu[1].chomp)
+		@prix = contenu[2].to_i
 	end
 	
 	# Méthode transformant un thème en une ligne de fichier.
@@ -57,9 +58,10 @@ class Theme
 		contenuFichier = ""
 		
 		# Ajout des autres champs du thème
-		contenuFichier += self.toString(@couleur1)
-		contenuFichier += ";" + self.toString(@couleur2)
-		contenuFichier += ";" + self.toString(@couleur3)
+		# contenuFichier += self.toString(@couleur1)
+		# contenuFichier += ";" + self.toString(@couleur2)
+		# contenuFichier += ";" + self.toString(@couleur3)
+		contenuFichier += @nom
 		contenuFichier += ";" + self.toCaractereBooleen(@debloque)
 		contenuFichier += ";" + @prix.to_s
 		contenuFichier += "\n"
@@ -85,46 +87,46 @@ class Theme
 	
 	# Méthode d'affichage
 	def to_s() 
-		return "Couleur 1 = " + toString(@couleur1) + "\nCouleur 2 = " + toString(@couleur2) + "\nCouleur 3 = " + toString(@couleur3) + "\nDébloqué = #{@debloque}\nPrix = #{@prix}$"
+		return @nom #"Couleur 1 = " + toString(@couleur1) + "\nCouleur 2 = " + toString(@couleur2) + "\nCouleur 3 = " + toString(@couleur3) + "\nDébloqué = #{@debloque}\nPrix = #{@prix}$"
 	end
 	
 	# Méthode d'affichage de couleur
-	def toString(couleur)
+	# def toString(couleur)
 	
-		case couleur
-			when :BLANC
-				return "blanc"
-			when :NOIR
-				return "noir"
-			when :BLEU
-				return "bleu"
-			when :VERT
-				return "vert"
-			when :JAUNE
-				return "jaune"
-			when :ROUGE
-				return "rouge"
-		end
-	end
+	# 	case couleur
+	# 		when :BLANC
+	# 			return "blanc"
+	# 		when :NOIR
+	# 			return "noir"
+	# 		when :BLEU
+	# 			return "bleu"
+	# 		when :VERT
+	# 			return "vert"
+	# 		when :JAUNE
+	# 			return "jaune"
+	# 		when :ROUGE
+	# 			return "rouge"
+	# 	end
+	# end
 	
 	# Méthode dde transformation de chaîne de caractère en couleur
-	def toCouleur(string)
+	# def toCouleur(string)
 	
-		case string
-			when "blanc"
-				return :BLANC
-			when "noir"
-				return :NOIR
-			when "bleu"
-				return :BLEU
-			when "vert"
-				return :VERT
-			when "jaune"
-				return :JAUNE
-			when "rouge"
-				return :ROUGE
-		end
-	end
+	# 	case string
+	# 		when "blanc"
+	# 			return :BLANC
+	# 		when "noir"
+	# 			return :NOIR
+	# 		when "bleu"
+	# 			return :BLEU
+	# 		when "vert"
+	# 			return :VERT
+	# 		when "jaune"
+	# 			return :JAUNE
+	# 		when "rouge"
+	# 			return :ROUGE
+	# 	end
+	# end
 	
 	# Méthode transformant le caractère reçu du fichier en booléen
 	def toBooleen(caractere)

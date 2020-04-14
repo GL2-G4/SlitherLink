3d80c95f2ac30bfbc9fb62334ab289e1dd7ef85a
CPietJa <tibane.galbrun@gmail.com>
2020-04-14 17:23:53 +0200

    + Mode Challenge

--- UI/MenuChallenge.rb
+++ UI/MenuChallenge.rb
@@ -1,8 +1,12 @@
 require "gtk3"
 
+require_relative './ChallengeManager.rb'
+require_relative './Challenge.rb'
+
 class MenuChallenge < Gtk::Box
 
     private_class_method :new
+    attr_reader :challenges
 
     def MenuChallenge.creer(gMenu, menuPere)
         new(gMenu, menuPere)
@@ -14,7 +18,6 @@ class MenuChallenge < Gtk::Box
         @pere = menuPere
         @vBox = Gtk::Box.new(:vertical)
         @vBox2 = Gtk::ButtonBox.new(:vertical)
-        @listeBoutons = Array.new($nbPuzzles);
 
         @scrolled = Gtk::ScrolledWindow.new
         @scrolled.set_policy(:never, :automatic)
@@ -30,19 +33,38 @@ class MenuChallenge < Gtk::Box
         @titre.style_context.add_class("titre")
         @vBox2.add(@titre)
 
-        @listeBoutons.each_index { |index|
+        path = File.expand_path(File.dirname(__FILE__))
+
+        @challenges = ChallengeManager.charger(path + "/../Grilles/challenges.json")
+        #puts @challenges.to_s
+        
+        @challenges.challenges["challenges"].each { |c|
             boxBouton = Gtk::ButtonBox.new(:horizontal)
             boxBouton.set_width_request($longListe)
             border = Gtk::Frame.new()
             boxBouton.set_border_width(10)
-            textBox = Gtk::Label.new("coucou")
-            bouton = Gtk::Button.new()
-            bouton.set_label("Puzzle n°" + index.to_s)
-            bouton.signal_connect "clicked" do |_widget|
-                puts "Jouer au puzzle n°" + index.to_s
+            # Titre
+            textBox = Gtk::Label.new(c["nom"])
+            # Temps
+            t = c["m_temps"]
+            if(t != 0)
+                hour = t/60/60
+                min = (t - (hour*60))/60
+                sec =  (t - (hour*60) - (min*60))
+                #puts "#{hour}:#{min}:#{sec}"
+                temps = Gtk::Label.new(sprintf("%02d:%02d",min,sec))
+            else
+                temps = Gtk::Label.new("--:--")
             end
-            @listeBoutons[index] = bouton
+            # Bouton JOUER
+            bouton = Gtk::Button.new()
+            bouton.set_label("Jouer")
+            bouton.signal_connect "clicked" do |_widget|
+                #puts "Jouer au challenge"
+                Challenge.new(@gMenu,self,c)
+            end
             boxBouton.add(textBox)
+            boxBouton.add(temps)
             boxBouton.add(bouton)
             border.add(boxBouton)
             @vBox2.add(border)
